1bd7f8a110f1ab97ed360cddc1f54f33f898a32e
wandouduoduo <wandouduoduo@163.com>
2019-06-29 23:27:57 +0800

    django模板项目提交

---
+++ apps/users/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class UsersConfig(AppConfig):
+    name = 'users'
+    # app名字后台显示中文
+    verbose_name = "用户管理"
---
+++ db_tools/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @File  : __init__.py.py
+# @Author: Sun
+# @Date  : 2019/6/29
